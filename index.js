8e3ad731fb15181d29833e55c374d79b62bc7d4e
yassineDevox <yassine.rassy1@gmail.com>
2022-02-16 14:23:40 +0100

    done

--- index.js
+++ index.js
@@ -1,88 +1,65 @@
-const express = require("express")
-const cors = require("cors")
-const bp = require("body-parser")
-const mysql = require("mysql")
-const app = express()
+const express = require("express");
+const mysql = require("mysql");
 
-//body could have diff types 
-app.use(bp.urlencoded({ extended: true }))
-//looks at requests where the Content-Type: application/json (Header)
-app.use(bp.json())
-//appliquer le cors comme middle ware 
-app.use(cors())
+const app = express();
+
+app.listen("9000", () => {
+  console.log("Server started on port 9000 😇");
+});
 
 //----DATABASE----
-//create the connection
+//create the connection with xamp(mysql)
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todo_dabase'
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tododb",
+});
 
-//Connect
-db.connect((err) => {
-    if (err) throw err
-    console.log("Mysql connected...")
-})
-
-app.listen('9000', () => {
-    console.log('Server started on port 9000 😇');
-})
-
-//ecouter les requetes http 
-app.get("/create-todo-table", (requestHTTP, responseHTTP) => {
-
-    const onCreateTodoTableQuery = (err, QueryResult) => {
-
-        if (err) throw err
-        else {
-            responseHTTP.send("table todo created and joined with user via (UserId column FK ) 😃 !")
-            console.log(QueryResult);
-        }
+//lorsque lutilisateur invoke cet url
+app.get("/api/creer-table-user", (requestHTTP, responseHTTP) => {
+  // console.log("create user table ...")
+  //utiliser db pour executer des reqs
+  db.query(
+    `CREATE TABLE users 
+        ( 
+            id INT(11) NOT NULL AUTO_INCREMENT , 
+            firstname VARCHAR(60) NOT NULL , 
+            lastname VARCHAR(60) NOT NULL , 
+            avatarURL VARCHAR(60) NOT NULL , 
+            email VARCHAR(60) NOT NULL,
+            role ENUM('DEV','LEADER','MANAGER') NOT NULL DEFAULT 'DEV' , 
+            PRIMARY KEY (id), UNIQUE email (email) 
+        ) `,
+    (errorDB,resultatQury) => {
+        if(errorDB) throw errorDB
+      //envoyer une reponse vers le client
+      responseHTTP.send("user table created 😃 !");
     }
-    
-    db.query(
-        `CREATE TABLE todos 
+  );
+});
+
+//lorsque lutilisateur invoke cet url
+app.get("/api/creer-table-task", (requestHTTP, responseHTTP) => {
+  // console.log("create todo table ...")
+  //utiliser db pour executer des reqs
+  db.query(
+    `CREATE TABLE tasks 
                 ( 
                     id INT(11) NOT NULL AUTO_INCREMENT , 
                     title VARCHAR(60) NOT NULL , 
                     description VARCHAR(225) NOT NULL , 
                     status ENUM('DONE','CANCELED','INPROGRESS','TODO') NOT NULL DEFAULT 'TODO' , 
-                    createAt DATETIME NOT NULL , 
+                    startedAt DATETIME NOT NULL , 
                     doneAt DATETIME NOT NULL , 
                     userId int(11) NOT NULL,
                     PRIMARY KEY (id),
                     FOREIGN KEY (userId) REFERENCES users(id)
                 )
-
-        `,onCreateTodoTableQuery)
-})
-
-
-//ecouter les requetes http 
-app.get("/create-user-table", (requestHTTP, responseHTTP) => {
-
-    const onCreateUserTableQuery = (err, QueryResult) => {
-
-        if (err) throw err
-        else {
-            responseHTTP.send("table user created 😃 !")
-            console.log(QueryResult);
-        }
+     `,
+    () => {
+      //envoyer une reponse vers le client
+      responseHTTP.send("task table created 😃 !");
     }
-
-    db.query(
-        `CREATE TABLE users 
-                ( 
-                    id INT(11) NOT NULL AUTO_INCREMENT , 
-                    firstname VARCHAR(60) NOT NULL , 
-                    lastname VARCHAR(60) NOT NULL , 
-                    avatarURL VARCHAR(60) NOT NULL , 
-                    email VARCHAR(60) NOT NULL,
-                    role ENUM('DEV','LEADER','MANAGER') NOT NULL DEFAULT 'DEV' , 
-                    PRIMARY KEY (id), UNIQUE email (email) 
-                ) 
-        `, onCreateUserTableQuery)
-})
-
+  );
+});
